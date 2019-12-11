0f3291b33e9ead9e9934c463feae810af9f9769f
stevevovchyna <steve.vovchyna@gmail.com>
2019-12-11 18:29:47 +0200

    requests are now in the different file, token and search working properly so far

--- SwiftyCompanion/SwiftyCompanion/FirstViewController.swift
+++ SwiftyCompanion/SwiftyCompanion/FirstViewController.swift
@@ -14,8 +14,6 @@ class FirstViewController: UIViewController {
     
     var token : String?
     var userData : JSON?
-    
-    let parameters = ["grant_type" : "client_credentials", "client_id" : "fde0aa7c5eec519a007e41ef35a88a63cd59341544fbdfdc17c9d7762fceb481", "client_secret" : "254a6eaa192270af710cbac115c6ab75a5df68edab025c76913d7494d60100c8"]
 
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var searchUserDataButtonLabel: UIButton!
@@ -23,52 +21,58 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
-//        searchUserDataButtonLabel.isHidden = true
-        token = "8a1c6ac86598632cc28e8699ad91ebc4c70f309d40991c3940b463df1b3084bc"
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-//        DispatchQueue.main.async {
-//            Alamofire.request("https://api.intra.42.fr/oauth/token", method: .post, parameters: self.parameters).responseJSON {
-//                response in
-//                    if response.result.isSuccess {
-//                        print(response)
-//                        let tokenData : JSON = JSON(response.result.value!)
-//                        self.token = tokenData["access_token"].stringValue
-//                        UIView.animate(withDuration: 0.5) {
-//                            self.searchUserDataButtonLabel.isHidden = false
-//                        }
-//                    } else {
-//                        print("Error: Seems like the service is unavailable right now)")
-//                    }
-//            }
-//        }
-    }
-
-    @IBAction func searchUserDataButton(_ sender: UIButton) {
-        print("tut")
-        if searchTextField.text != "" {
-            let urlQuery = "https://api.intra.42.fr/v2/users/" + searchTextField.text!
-            let params = ["client_id" : "fde0aa7c5eec519a007e41ef35a88a63cd59341544fbdfdc17c9d7762fceb481", "client_secret" : "254a6eaa192270af710cbac115c6ab75a5df68edab025c76913d7494d60100c8"]
-            let header = ["Authorization" : "Bearer " + self.token!]
-            Alamofire.request(urlQuery, method: .get, parameters: params, headers: header).responseJSON {
-                response in
-                self.userData = JSON(response.result.value!)
-                if response.result.isSuccess, self.userData!.count > 0 {
-                    self.performSegue(withIdentifier: "showUserData", sender: self)
-                } else {
-                    print("Error when requesting user data")
+        searchUserDataButtonLabel.isHidden = true
+        OAuthManager.getToken { result in
+            switch result {
+            case .success(let newToken):
+                self.token = (newToken["access_token"] as! String)
+                UIView.animate(withDuration: 1) {
+                    self.searchUserDataButtonLabel.isHidden = false
                 }
+            case .failure(let error):
+                print(error)
+                self.presentAlert(text: error.localizedDescription)
             }
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    
+    @IBAction func searchUserDataButton(_ sender: UIButton) {
+        if searchTextField.text != "" {
+            OAuthManager.searchUser(query: searchTextField.text!, token: token!) { result in
+                switch result {
+                case .success(let userData):
+                    if userData.count > 0 {
+                        self.userData = JSON(userData)
+                        self.performSegue(withIdentifier: "showUserData", sender: self)
+                    } else {
+                        self.presentAlert(text: "User not found!")
+                    }
+                case .failure(let error):
+                    print(error)
+                    self.presentAlert(text: error.localizedDescription)
+                }
+            }
+        } else {
+            self.presentAlert(text: "Please enter some text to make a query")
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("tam")
         if segue.identifier == "showUserData" {
             let constroller = segue.destination as! SecondViewController
             constroller.userData = userData
         }
+    }
+    
+    func presentAlert(text: String) {
+        let alert = UIAlertController(title: "Error", message: text, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
---
+++ SwiftyCompanion/SwiftyCompanion/OAuthManager.swift
@@ -0,0 +1,51 @@
+//
+//  OAuthManager.swift
+//  SwiftyCompanion
+//
+//  Created by Steve Vovchyna on 11.12.2019.
+//  Copyright © 2019 Steve Vovchyna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class OAuthManager {
+    
+    static let clientID = "fde0aa7c5eec519a007e41ef35a88a63cd59341544fbdfdc17c9d7762fceb481"
+    static let clientSecret = "254a6eaa192270af710cbac115c6ab75a5df68edab025c76913d7494d60100c8"
+    static var parameters = ["client_id" : clientID, "client_secret" : clientSecret]
+    
+    static func getToken(completionHandler: @escaping (Result<[String: Any]>) -> Void) {
+        let url = "https://api.intra.42.fr/oauth/token"
+        var params = self.parameters
+        params["grant_type"] = "client_credentials"
+        Alamofire.request(url, method: .post, parameters: params).responseJSON { response in
+                switch response.result {
+                case .success(let value as [String: Any]):
+                    completionHandler(.success(value))
+                case .failure(let error):
+                    completionHandler(.failure(error))
+                default:
+                    fatalError("received non-dictionary JSON response")
+                }
+        }
+    }
+    
+    static func searchUser(query: String, token: String, completionHandler: @escaping (Result<[String: Any]>) -> Void) {
+        let urlQuery = "https://api.intra.42.fr/v2/users/" + query
+        let header = ["Authorization" : "Bearer " + token]
+        Alamofire.request(urlQuery, method: .get, parameters: self.parameters, headers: header).responseJSON { response in
+            switch response.result {
+            case .success(let value as [String: Any]):
+                completionHandler(.success(value))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            default:
+                fatalError("received non-dictionary JSON response")
+            }
+        }
+    }
+    
+}
